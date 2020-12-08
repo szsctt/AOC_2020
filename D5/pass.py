d87c291fc3ad6a1634a225491dbf24bba3447ddf
Suzanne Scott <suzannescott@Suzannes-MacBook-Air.local>
2020-12-08 17:18:15 +1000

    update

--- D5/pass.py
+++ D5/pass.py
@@ -20,19 +20,52 @@
 
 import pdb
 
-def seat_id(id):
-	col = column(id[0:7])
-	row = row(id[7:])
-	return row*8+col
+def main():
+	max_id = 0
+	ids = []
+	with open('input', 'r') as handle:
+		# get ids for each seat in list
+		for row in handle:
+			sid = seat_id(row.strip())
+			max_id = max(max_id, sid)
+			ids.append(sid)
+			
+	print(f"the maximum seat id was {max_id}")
 	
-def col(col_str):
-	num = 0b1111111
-	for i in range(6, 0, -1):
-		if col_str[i] == 'F':
-			num = num << 1
-	print(f"{col_str}, {bin(num)}, {num}")
+	# sort list and check for diff of 2
+	ids = sorted(ids)
+	i = ids[0]
+	for j in sorted(ids[1:]):
+		if i != j-1:
+			print(f"gap at seat {i}, so seat is {i+1}")
+		i = j
+
+def seat_id(id):
+	rownum = row(id[0:7])
+	colnum = column(id[7:])
+	return rownum*8+colnum
+	
+def row(row_str):
+	"""
+	convert row string to binary number where 'F' is 0 and 'B' is 1
+	this gives row number
+	"""
+	num = 0
+	for i in range(len(row_str)):
+		if row_str[i] == 'B':
+			num += 1 << (len(row_str) - i - 1)
 	return num
 	
-col('BFFFBBF')
-col('FFFBBBF')
-col('BBFFBBF')
+def column(col_str):
+	"""
+	convert column string to binary number where 'L' is 0 and 'R' is 1
+	this gives column number
+	"""
+	num = 0
+	for i in range(len(col_str)):
+		if col_str[i] == 'R':
+			num += 1 << (len(col_str) - i - 1)
+	return num
+
+if __name__ == "__main__":
+	main()
